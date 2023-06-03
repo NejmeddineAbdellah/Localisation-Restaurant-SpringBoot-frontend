97a2d6f0cde36c9b66ebbcc92488ab99da634b6d
abdellah nejmeddine <crissnajm1998@gmail.com>
2023-06-03 12:09:55 +0100

    last V

--- src/services/services/auth.service.js
+++ src/services/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8040/api/auth/";
+const API_URL = "https://local-restau-springboot-backend-production.up.railway.app/api/auth";
 
 class AuthService {
   login(username, password) {
